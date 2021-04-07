00e6dc96dae3f28c2f05ab4e31e92ebb69817256
melaniekwak <melaniekwak13@gmail.com>
2021-04-06 18:20:02 -0700

    apps

---
+++ static/js/app_1.js
@@ -0,0 +1,44 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+  }
+
+// Converted to an arrow function
+addition = (a, b) => a + b;
+
+// Original doubleAddition function
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+
+doubleAddition = (c, d) => addition(c, d) * 2;
+doubleAddition = (33, 25)
+
+
+
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
